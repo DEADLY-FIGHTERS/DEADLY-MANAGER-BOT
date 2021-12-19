135d7b282aeca1c698a8e86a07352e56f8d5dd38
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-12-19 22:24:33 +0530

    Update mongo.py

--- Zaid/mongo.py
+++ Zaid/mongo.py
@@ -7,7 +7,7 @@ from Zaid import MONGO_DB_URI
 
 
 MONGO_PORT = "27017"
-MONGO_DB_URI = "postgres://txfpgmmc:U4lmTyueK0grG-VzLBRnOb7lRHxQNsyx@topsy.db.elephantsql.com/txfpgmmc"
+MONGO_DB_URI = "mongodb+srv://music:music@cluster0.cpkaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 MONGO_DB = "ZaidRobot"
 
 
